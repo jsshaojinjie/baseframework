d70800279ac807a5d45ff880dac926170e178ae6
jsshaojinjie <jsshaojinjie@aliyun.com>
2020-07-08 10:50:36 +0800

    添加swagger2文档控件

--- src/main/java/com/baseframework/config/security/WebSecurityConfig.java
+++ src/main/java/com/baseframework/config/security/WebSecurityConfig.java
@@ -45,11 +45,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/images/**", "/webjars/**", "/v2/api-docs", "/configuration/ui", "/configuration/security").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
                 .addFilter(new JWTLoginFilter(authenticationManager()))
-                .addFilter(new JWTAuthenticationFilter(authenticationManager(),userDetailsService));
+                .addFilter(new JWTAuthenticationFilter(authenticationManager(), userDetailsService));
     }
 
     @Override
